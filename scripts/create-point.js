e569178ac6b0a27477d84c6ffda138533e278472
ViniciusSCG <vinicarvalho23@gmail.com>
2020-06-03 20:43:44 -0300

    Nlw

---
+++ scripts/create-point.js
@@ -0,0 +1,94 @@
+
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( (res) => { return res.json() })
+    .then( states => {
+
+        for( const state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+
+        
+    })
+}
+
+populateUFs();
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true;
+
+    fetch(url)
+    .then( (res) => { return res.json() })
+    .then( cities => {
+
+        
+
+        for( const city of cities){
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+
+        
+        
+        citySelect.disabled = false;
+        
+    })
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+// Itens de coleta
+// pegar todos os li's
+
+const itensToCollect = document.querySelectorAll(".itens-grid li")
+
+for (const item of itensToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectItens = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    //adicionar ou remover uma classe com JS
+    
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    // verificar se excistem itens selecionados, se sim
+    //pegar os itens selecionados
+
+    const alreadySelected = selectItens.findIndex( (item) => {
+        
+        return itemFound = item == itemId
+    })
+
+    //se ja estiver selecionado, tirar da selecao
+
+    if(alreadySelected  >= 0){
+        //tirar da selecao
+        const filteredItens = selectItens.filter( item =>{
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent;
+        })
+    }
+
+    // se não estiver selecionado, adicionar à seleção
+
+    // atualizar o campo econdido com os itensselecionados
+}
